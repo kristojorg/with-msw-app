0ddca89925ab9c209b2a26491e4fc3bcfe977029
toxiapo <toxiapo@gmail.com>
2022-11-07 10:23:05 -0500

    with edge

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,13 +1,19 @@
 async function initMocks() {
-  if (typeof window === 'undefined') {
-    const { server } = await import('./server')
-    server.listen()
+  if (process.env.NEXT_RUNTIME) {
+    const { edge } = await import("./edge");
+    console.log("Mookcing Edge Function");
+    edge.listen();
+  } else if (typeof window === "undefined") {
+    const { server } = await import("./server");
+    console.log("Mookcing Sever Function");
+    server.listen();
   } else {
-    const { worker } = await import('./browser')
-    worker.start()
+    const { worker } = await import("./browser");
+    console.log("Mookcing Browser Function");
+    worker.start();
   }
 }
 
-initMocks()
+initMocks();
 
-export {}
+export {};
